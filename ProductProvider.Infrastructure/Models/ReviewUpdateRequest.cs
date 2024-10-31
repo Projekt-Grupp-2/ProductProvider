815b0d07261f056ab33aa102c1e7e5e1963e84d6
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-10-31 13:46:32 +0100

    CRUD för ReviewEntity

---
+++ ProductProvider.Infrastructure/Models/ReviewUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using ProductProvider.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductProvider.Infrastructure.Models;
+
+public  class ReviewUpdateRequest
+{
+    public Guid Id { get; set; }
+
+    public Guid? ProductId { get; set; }
+
+    public int? Stars { get; set; }
+
+    public string? Text { get; set; }
+}
