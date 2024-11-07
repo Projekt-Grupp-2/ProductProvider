b3025701f1683d74d361fe606411ec2f5b9f9447
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-11-07 08:57:47 +0100

    Category functions

--- ProductProvider/Functions/CategoryFunctions.cs
+++ ProductProvider/Functions/CategoryFunctions.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using ProductProvider.Infrastructure.Models;
 using ProductProvider.Infrastructure.Services;
+using System.Threading;
 
 namespace ProductProvider.Functions
 {
@@ -19,7 +20,7 @@ namespace ProductProvider.Functions
             _logger = logger;
         }
 
-        [Function("CategoryFunctions")]
+        [Function("CreateCategoryFunction")]
         public async Task<HttpResponseData> CreateCategory([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("CreateCategory");
@@ -52,41 +53,121 @@ namespace ProductProvider.Functions
         }
 
 
-        
-
-
-        /*
-        [Function("CreateUniqueProduct")]
-        public async Task<HttpResponseData> CreateUniqueProduct(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "warehouse/create")] HttpRequestData req,
-            FunctionContext executionContext)
+        [Function("GetOneCategoryIdFunction")]
+        public async Task<HttpResponseData> GetCategoryId(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "categories/{categoryName}")] HttpRequestData req,
+            string categoryName, FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("CreateUniqueProduct");
+            var logger = executionContext.GetLogger("GetOneCategoryId");
 
             try
             {
-                var uniqueProduct = await req.ReadFromJsonAsync<WarehouseModel>();
+                var categoryId = await _categoryService.GetCategoryIdByNameAsync(categoryName);
 
-                if (uniqueProduct == null)
+                if (categoryId == null)
                 {
-                    logger.LogError("Product data is missing or invalid.");
-                    return req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                    return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
                 }
 
-                var createdProduct = await _warehouseService.CreateUniqueProduct(uniqueProduct);
 
-                if (createdProduct == null)
-                {
-                    return req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
-                }
+                var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+                await response.WriteAsJsonAsync(categoryId);
 
-                return req.CreateResponse(System.Net.HttpStatusCode.Created);
+
+                return response;
             }
             catch (Exception ex)
             {
                 logger.LogError($"An error occurred: {ex.Message}");
                 return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
             }
-        }*/
+        }
+
+        [Function("GetAllCategories")]
+        public async Task<HttpResponseData> GetAllCategories(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "categories/all")] HttpRequestData req, FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("GetAllCategories");
+
+            try
+            {
+                var allCategories = await _categoryService.GetAllCategoriesAsync();
+
+                if(!allCategories.Any())
+                {
+                    return req.CreateResponse(System.Net.HttpStatusCode.NoContent);
+                }
+
+                var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+                await response.WriteAsJsonAsync(allCategories);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occurred: {ex.Message}");
+                return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [Function("UpdateCategory")]
+        public async Task<HttpResponseData> UpdateCategory(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "categories/update")] HttpRequestData req, 
+            CategoryModel categoryToUpdate, FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("UpdateCategory");
+
+            try
+            {
+
+                var updatedCategory = await _categoryService.UpdateCategoryAsync(categoryToUpdate);
+
+                if (updatedCategory == null)
+                {
+                    return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+                }
+
+                return req.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occurred: {ex.Message}");
+                return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        [Function("DeleteCategory")]
+        public async Task<HttpResponseData> DeleteCategory(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "categories/delete/{categoryId}")] HttpRequestData req,
+            string categoryId, FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("DeleteCategory");
+
+            try
+            {
+                if (!Guid.TryParse(categoryId, out Guid parsedCategoryId))
+                {
+                    // If parsing fails, return a BadRequest with a helpful message
+                    var badRequestResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                    await badRequestResponse.WriteStringAsync($"Invalid productId format. Expected a valid GUID, but received: {categoryId}");
+                    return badRequestResponse;
+                }
+
+                var deletedCategory = await _categoryService.DeleteCategoryAsync(parsedCategoryId);
+
+                if(!deletedCategory)
+                {
+                    return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+                }
+
+                return req.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occurred: {ex.Message}");
+                return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
     }
 }
